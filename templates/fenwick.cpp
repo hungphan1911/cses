d5931e12d7706fa02797d800943cc3ea9a8b1c82
hungphan1911 <hungphanquocviet@gmail.com>
2026-09-10 21:10:08 -0600

    Remove unneeded stuff in fenwick template

--- templates/fenwick.cpp
+++ templates/fenwick.cpp
@@ -3,18 +3,6 @@ using namespace std;
 #define int long long
 #define ll long long
 #define endl '\n'
-const int MOD = 1e9+7;
-const int INF = 1e18;
-
-template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
-template<class T> using max_heap = priority_queue<T>;
-
-// ===================== Debug =====================
-template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
-template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
-template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
-template<typename T> void print(T && x) {cout << x << "\n";}
-template<typename... Args> void print(Args&&... args) {((cout << args << " "), ...);cout << "\n";}
 
 // usage: dynamic range sum query
 // example: https://cses.fi/problemset/task/1648
